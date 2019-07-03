3fef111d1813ed5c82ea7644d0590d390a7aa04e
Ubuntu <ubuntu@ip-172-31-22-55.ap-southeast-1.compute.internal>
2019-07-03 09:09:12 +0000

    Added Get Taxi button and show location of one taxi

--- scripts/app.js
+++ scripts/app.js
@@ -1,20 +1,40 @@
 var map;
 var endpoint = "https://api.data.gov.sg/v1/transport/taxi-availability"
 
+$(function(){
+    $("#get-taxi-button").click(function(){
+      // async - how do we wait for the data to finish
+      // loading before executing the next task?
+      getDataFromEndpointAsync(function(data){
+          let taxiPos = {
+            lat: data[0][1],
+            lng: data[0][0]
+          };
+          console.log(taxiPos);
+          new google.maps.Marker({
+              position: taxiPos,
+              map: map
+          })
+      });
+      
+    })
+});
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8
+        center: { lat: 1.35, lng: 103.81 },
+        zoom: 12
     });
 }
 
 
 // data source
-function getDataFromEndpoint() {
+function getDataFromEndpointAsync(callback) {
     axios.get(endpoint)
         .then(function(response) {
             let results = response.data.features[0].geometry.coordinates;
+            // trigger the callback
+            callback(results);
             console.log(response)
         });
 }
