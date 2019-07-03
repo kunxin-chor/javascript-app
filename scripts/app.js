9856cc725745ba37e85cb27b24f422956ae76e5a
Ubuntu <ubuntu@ip-172-31-22-55.ap-southeast-1.compute.internal>
2019-07-03 08:46:51 +0000

    Added the data source function

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,20 @@
-  var map;
+var map;
+var endpoint = "https://api.data.gov.sg/v1/transport/taxi-availability"
 
-  function initMap() {
-      map = new google.maps.Map(document.getElementById('map'), {
-          center: { lat: -34.397, lng: 150.644 },
-          zoom: 8
-      });
-  }
-  
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8
+    });
+}
+
+
+// data source
+function getDataFromEndpoint() {
+    axios.get(endpoint)
+        .then(function(response) {
+            let results = response.data.features[0].geometry.coordinates;
+            console.log(response)
+        });
+}
